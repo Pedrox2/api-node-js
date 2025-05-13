ddce77104eb81606924816aea6d9f11c62d77a8b
Pedrox2 <phfernandesmartins669@gmail.com>
2025-05-13 11:45:06 -0300

    aula 13-05

--- src/controllers/categorias.js
+++ src/controllers/categorias.js
@@ -3,10 +3,20 @@ const db = require('../dataBase/connection');
 module.exports = {
     async listarCategorias(request, response) {
         try {
+
+            const sql= `
+            SELECT 
+            categ_id, categ_nome, 
+            categ_icone FROM categorias;
+            `;
+
+            const [rows] = await db.query(sql);
+
             return response.status(200).json({
                 sucesso: true, 
-                mensagem: 'Lista de Categorias', 
-                dados: null
+                mensagem: 'Lista de Categorias',
+                itens: rows.length ,
+                dados: rows
             });
         } catch (error) {
             return response.status(500).json({
